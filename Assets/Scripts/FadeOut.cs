b20e40242c778917f4e68465f979ed9362e9e8a3
lemon <jackson.w.lam@gmail.com>
2025-02-13 03:42:44 -0600

    Working Prototype
    Added multiple levels
    Win condition

---
+++ Assets/Scripts/FadeOut.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class FadeOut : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public CanvasGroup fadeCanvas; // UI Canvas Group for fading
+    
+    void Start()
+    {
+        StartCoroutine(FadeScreen(0f));
+    }
+    
+    IEnumerator FadeScreen(float targetAlpha)
+    {
+        Debug.Log("Coroutine started");
+        float startAlpha = fadeCanvas.alpha;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            fadeCanvas.alpha = Mathf.Lerp(startAlpha, targetAlpha, elapsedTime / fadeDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        fadeCanvas.alpha = targetAlpha;
+    }
+}
