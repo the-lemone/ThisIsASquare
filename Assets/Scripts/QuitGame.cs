f08558fca7514f2e200c95212ffbdbe7cbc8ae43
lemon <jackson.w.lam@gmail.com>
2025-05-21 20:54:11 -0500

    Changes for Dreamhack
    - Added my own music
     - 30 second loop to be played in the background
    - Adjusted win material to be brighter
    - Adjusted quit game script to quit only if standalone, so it won't crash WebGL builds
    - Started 1 new level

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -5,10 +5,12 @@ public class QuitGame : MonoBehaviour
 {
     void Update()
     {
+        #if UNITY_STANDALONE || UNITY_EDITOR
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
         }
+        #endif
         if (Input.GetKeyDown(KeyCode.Backspace))
         {
             SceneManager.LoadScene("Level 0.1");
